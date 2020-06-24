4af14020934ceb22b83f4480299782efeae85980
Daniel Simão da Silva <simaodeveloper@gmail.com>
2020-06-24 15:04:59 -0300

    chore: upload initial structure

---
+++ src/app/elements/elements.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ElementsRoutingModule } from './elements-routing.module';
+import { ElementsHomeComponent } from './elements-home/elements-home.component';
+import { PlaceholderComponent } from './placeholder/placeholder.component';
+import { TimesDirective } from './times.directive';
+
+
+@NgModule({
+  declarations: [ElementsHomeComponent, PlaceholderComponent, TimesDirective],
+  imports: [
+    CommonModule,
+    ElementsRoutingModule
+  ],
+})
+export class ElementsModule { }
